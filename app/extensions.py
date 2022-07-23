addbd399fa913f5245e4e3e6fa3947a2b45aee64
Divya <kumaridivya.149d@gmail.com>
2022-07-23 12:54:48 +0530

    Commit for changing DB details

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,4 @@
 from flask_pymongo import PyMongo
 
 # Setup MongoDB here
-mongo = PyMongo(uri="mongodb+srv://root-mongodb:ROOTMongoDB123@cluster0.ybfir.mongodb.net/?retryWrites=true&w=majority")
+mongo = PyMongo(uri="mongodb+srv://{USER_NAME}:{PASSWORD}@{ClusterOrServerURL}/?retryWrites=true&w=majority")
